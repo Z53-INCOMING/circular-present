97b049a0f2c522a82bb308a4db1d0909c50b4ab6
Tessimal <75651517+Z53-INCOMING@users.noreply.github.com>
2025-06-26 12:55:38 -0700

    uy flipping

--- circle.gd
+++
@@ -1,6 +0,0 @@
-extends RigidBody2D
-
-var color := Color.WHITE
-
-func _ready():
-	$Visual.color = color
--- circular_present_object.gd
+++ circular_present_object.gd
@@ -1,6 +1,7 @@
 extends Node2D
 
 @onready var circular_object_scene := preload("res://circle.tscn")
+@onready var circular_visual_scene := preload("res://circle_visual.tscn")
 
 @export var color: Color = Color.WHITE
 
@@ -12,6 +13,8 @@ var time_stiffness := 1.0
 
 var selected := -1
 
+var uy_flipped := false
+
 func _ready():
 	present_resolution = get_parent().present_resolution
 	present_radius = get_parent().present_radius
@@ -22,9 +25,14 @@ func _ready():
 		circular_object.collision_layer = int(pow(2.0, i))
 		circular_object.collision_mask = int(pow(2.0, i))
 		
-		circular_object.color = color
-		
 		add_child(circular_object)
+	
+	for i in present_resolution:
+		var circular_visual: Polygon2D = circular_visual_scene.instantiate()
+		
+		circular_visual.color = color
+		
+		add_child(circular_visual)
 
 func _process(delta):
 	var camera_u: float = get_parent().camera_u
@@ -56,8 +64,18 @@ func _process(delta):
 		
 		current.apply_central_impulse((previous_force + next_force) * 0.5 * delta * time_stiffness)
 		
-		# Fade out the object if it's far away on the u axis
-		current.modulate.a = pow(1.0 - absf(angle_difference((float(i) / float(present_resolution)) * TAU - PI, camera_u - PI) / PI), 2.0)
+		# Update the polygon
+		var polygon: Polygon2D = get_child(i + present_resolution)
+		
+		polygon.global_transform = current.global_transform
+		polygon.modulate.a = pow(1.0 - absf(angle_difference((float(i) / float(present_resolution)) * TAU - PI, camera_u - PI) / PI), 2.0)
+		
+		if uy_flipped:
+			polygon.global_position.y = (1.0 - (i / float(present_resolution - 1))) * 1440.0
+			polygon.modulate.a = pow(1.0 - absf((current.global_position.y - ((camera_u / TAU) * 1440.0)) / 1440.0), 2.0)
+	
+	if Input.is_action_just_pressed("ui_accept"):
+		uy_flipped = !uy_flipped
 	
 	if selected != -1:
 		var closest_to_mouse = get_child(selected)
@@ -69,9 +87,18 @@ func _input(event):
 		if event.button_index == MOUSE_BUTTON_LEFT:
 			if event.pressed:
 				var camera_u_index := int((get_parent().camera_u / TAU) * present_resolution)
+				if uy_flipped:
+					camera_u_index = int((1.0 - (get_global_mouse_position().y / 1440.0)) * float(present_resolution))
+					if camera_u_index >= present_resolution:
+						camera_u_index = present_resolution - 1
+				
 				var closest_to_mouse = get_child(camera_u_index)
 				
-				if get_global_mouse_position().distance_to(closest_to_mouse.global_position) < 64.0:
-					selected = camera_u_index
+				if uy_flipped:
+					if absf(get_global_mouse_position().x - closest_to_mouse.global_position.x) < 64.0:
+						selected = camera_u_index
+				else:
+					if get_global_mouse_position().distance_to(closest_to_mouse.global_position) < 64.0:
+						selected = camera_u_index
 			else:
 				selected = -1
